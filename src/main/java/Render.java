d465208bbefb209e7622ad47bf38bcd7cb946e82
tpolanowski <polanowski.tomasz@gmail.com>
2015-06-16 23:00:43 +0200

    tram - final commit?

--- src/main/java/Render.java
+++ src/main/java/Render.java
@@ -52,7 +52,7 @@ public class Render implements GLEventListener{
         gl.glHint(gl.GL_PERSPECTIVE_CORRECTION_HINT, gl.GL_NICEST);  // Nice perspective corrections
         //gl.glEnable(GL2.GL_LIGHTING);
         //gl.glEnable(GL2.GL_LIGHT0);
-        gl.glEnable(GL2.GL_COLOR_MATERIAL);
+        //gl.glEnable(GL2.GL_COLOR_MATERIAL);
     }
 
     @Override
@@ -87,14 +87,14 @@ public class Render implements GLEventListener{
         gl.glTranslatef(-500, 0, -500);
 
         // terrain
-//        gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
-//        gl.glEnableClientState(GL2.GL_VERTEX_ARRAY);
-//        gl.glVertexPointer(3, GL.GL_FLOAT, 0, terrain.getVertexBuffer());
-//        gl.glEnableClientState(GL2.GL_COLOR_ARRAY);
-//        gl.glColorPointer(3, GL.GL_FLOAT, 0, terrain.getColorBuffer());
-//        gl.glDrawElements(GL.GL_TRIANGLES, terrain.getIndexCount(), GL2.GL_UNSIGNED_INT, terrain.getIndexBuffer());
-//        gl.glDisableClientState(GL2.GL_VERTEX_ARRAY);
-//        gl.glDisableClientState(GL2.GL_COLOR_ARRAY);
+        //gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
+        gl.glEnableClientState(GL2.GL_VERTEX_ARRAY);
+        gl.glVertexPointer(3, GL.GL_FLOAT, 0, terrain.getVertexBuffer());
+        gl.glEnableClientState(GL2.GL_COLOR_ARRAY);
+        gl.glColorPointer(3, GL.GL_FLOAT, 0, terrain.getColorBuffer());
+        gl.glDrawElements(GL.GL_TRIANGLES, terrain.getIndexCount(), GL2.GL_UNSIGNED_INT, terrain.getIndexBuffer());
+        gl.glDisableClientState(GL2.GL_VERTEX_ARRAY);
+        gl.glDisableClientState(GL2.GL_COLOR_ARRAY);
 
         // road
         drawTramRoad(gl, 4f);
@@ -114,10 +114,11 @@ public class Render implements GLEventListener{
         if (t == path.size()) {
             t = 0;
         }
-        drawTramFront(gl,0.04f);
-
-
-
+        drawTramFront(gl, 0.015f);
+        drawTramWagon(gl, 0.015f, 1300f, 30);
+        drawTramWagon(gl, 0.015f, 2600f, 40);
+        drawTramWagon(gl, 0.015f, 3900f, 45);
+        drawTramWagon(gl, 0.015f, 5200f, 49);
 
     }
 
@@ -189,7 +190,7 @@ public class Render implements GLEventListener{
             int deltaX = prev.getX() - x;
 
             float angle = (float) Math.toDegrees(Math.atan2(deltaY, deltaX));
-            System.out.println(200-angle);
+            //System.out.println(200-angle);
             gl.glRotatef(tramAngle+200-angle,0,1,0);
         }
         else {
@@ -347,20 +348,110 @@ public class Render implements GLEventListener{
         gl.glPopMatrix();
     }
 
+    private void drawTramWagon(GL2 gl, float s, float xStart, int delay) {
+        int x = path.get(t).getX();
+        int y = path.get(t).getY();
+
+        //gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_LINE);
+        gl.glPushMatrix();
+        gl.glTranslatef(x, 0, y);
+
+        // rotate the tram accordingly to the railroad
+        if (t > delay) {
+            x = path.get(t-delay+20).getX();
+            y = path.get(t-delay+20).getY();
+
+            Coord prev = new Coord(path.get(t-delay));
+            int deltaY = prev.getY() - y;
+            int deltaX = prev.getX() - x;
+
+            float angle = (float) Math.toDegrees(Math.atan2(deltaY, deltaX));
+            gl.glRotatef(tramAngle+200-angle,0,1,0);
+        }
+        else {
+            tramAngle = 162f;
+            gl.glRotatef(tramAngle,0,1,0);
+        }
+
+        gl.glColor3f(0.1f, 0.2f, 1f); // Dark Blue
+
+        // 1st wagon - right wall
+        gl.glBegin(gl.GL_QUADS);
+        gl.glVertex3f((xStart+300)*s,0,0);
+        gl.glVertex3f((xStart+1500)*s,0,0);
+        gl.glVertex3f((xStart+1500)*s,-600*s,0);
+        gl.glVertex3f((xStart+300)*s,-600*s,0);
+        gl.glEnd();
+
+        // 1st wagon - left wall
+        gl.glBegin(gl.GL_QUADS);
+        gl.glVertex3f((xStart+300)*s,0,-200*s);
+        gl.glVertex3f((xStart+1500)*s,0,-200*s);
+        gl.glVertex3f((xStart+1500)*s,-600*s,-200*s);
+        gl.glVertex3f((xStart+300)*s,-600*s,-200*s);
+        gl.glEnd();
+
+        // 1st wagon - roof
+        gl.glBegin(gl.GL_QUADS);
+        gl.glVertex3f((xStart+300)*s,-600*s,0);
+        gl.glVertex3f((xStart+1500)*s,-600*s,0);
+        gl.glVertex3f((xStart+1500)*s,-600*s,-200*s);
+        gl.glVertex3f((xStart+300)*s,-600*s,-200*s);
+        gl.glEnd();
+
+        // 1st wagon - back
+        gl.glBegin(gl.GL_QUADS);
+        gl.glVertex3f((xStart+1500)*s,0,0);
+        gl.glVertex3f((xStart+1500)*s,-600*s,0);
+        gl.glVertex3f((xStart+1500)*s,-600*s,-200*s);
+        gl.glVertex3f((xStart+1500)*s,0,-200*s);
+        gl.glEnd();
+
+
+        // 1st wagon - right windows
+        for (int i=0; i<5; i++) {
+            gl.glBegin(gl.GL_QUADS);
+            gl.glColor3f(0.6f, 0.7f, 1f); // Light Blue
+            gl.glVertex3f((xStart+(350+200*i))*s,-200*s,0.1f*s);
+            gl.glColor3f(0.4f, 0.6f, 0.9f); // Light Blue Tint 1
+            gl.glVertex3f((xStart+(500+200*i))*s,-200*s,0.1f*s);
+            gl.glColor3f(0.4f, 0.8f, 1.0f); // Light Blue Tint 2
+            gl.glVertex3f((xStart+(500+200*i))*s,-500*s,0.1f*s);
+            gl.glColor3f(0.6f, 0.7f, 1f); // Light Blue
+            gl.glVertex3f((xStart+(350+200*i))*s,-500*s,0.1f*s);
+            gl.glEnd();
+        }
+
+        // 1st wagon - left windows
+        for (int i=0; i<5; i++) {
+            gl.glBegin(gl.GL_QUADS);
+            gl.glColor3f(0.6f, 0.7f, 1f); // Light Blue
+            gl.glVertex3f((xStart+(450+200*i))*s,-200*s,-200.1f*s);
+            gl.glColor3f(0.4f, 0.6f, 0.9f); // Light Blue Tint 1
+            gl.glVertex3f((xStart+(600+200*i))*s,-200*s,-200.1f*s);
+            gl.glColor3f(0.4f, 0.8f, 1.0f); // Light Blue Tint 2
+            gl.glVertex3f((xStart+(600+200*i))*s,-500*s,-200.1f*s);
+            gl.glColor3f(0.6f, 0.7f, 1f); // Light Blue
+            gl.glVertex3f((xStart+(450+200*i))*s,-500*s,-200.1f*s);
+            gl.glEnd();
+        }
+        gl.glPopMatrix();
+    }
+
     private void drawTramRoad(GL2 gl, float size) {
         gl.glPolygonMode(GL2.GL_FRONT_AND_BACK, GL2.GL_FILL);
         gl.glPushMatrix();
 
         for(Coord coord : path) {
             gl.glBegin(gl.GL_QUADS);
-            gl.glColor3f(0.6f, 0.6f, 0.6f); // Gray
-            gl.glVertex3f(coord.getX()+size,0f,coord.getY()+size);
-            gl.glColor3f(0.6f, 0.6f, 0.6f); // Gray
-            gl.glVertex3f(coord.getX()+size,0f,coord.getY()-size);
-            gl.glColor3f(0.6f, 0.6f, 0.6f); // Gray
-            gl.glVertex3f(coord.getX()-size,0f,coord.getY()-size);
-            gl.glColor3f(0.6f, 0.6f, 0.6f); // Gray
-            gl.glVertex3f(coord.getX()-size,0f,coord.getY()+size);
+            gl.glColor3f(0.5f, 0.6f, 0.6f); // Gray
+            gl.glVertex3f(coord.getX()+size,-0.004f,coord.getY()+size);
+            gl.glColor3f(0.6f, 0.5f, 0.6f); // Gray
+            gl.glVertex3f(coord.getX()+size,-0.004f,coord.getY()-size);
+            gl.glColor3f(0.6f, 0.6f, 0.5f); // Gray
+            gl.glVertex3f(coord.getX()-size,-0.004f,coord.getY()-size);
+            gl.glColor3f(0.6f, 0.7f, 0.6f); // Gray
+            gl.glVertex3f(coord.getX()-size,-0.004f,coord.getY()+size);
             gl.glEnd();
         }
 
